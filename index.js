b9462bf4669c015430687faa91fea9d2e1b74d3e
starlight_caffeine <starlight-caffeine@posteo.org>
2023-09-29 15:29:51 +0200

    Add print support

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function preparePrint(map) {
-    
+function preparePrint() {
+    document.querySelector(".right").classList.toggle("hidden");
+    document.querySelector(".left").classList.toggle("full-width");
 }
