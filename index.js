bb8f7ca4d48646887cb7dc2816ffb5ca71d7926c
starlight_caffeine <starlight-caffeine@posteo.org>
2023-10-04 09:28:06 +0200

    Working MaplibreGL, some refactoring

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function preparePrint() {
     document.querySelector(".right").classList.toggle("hidden");
     document.querySelector(".left").classList.toggle("full-width");
 }
+
+console.log(maplibregl);
+        var map = new maplibregl.Map({
+          container: 'map',
+          style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+          center: [-74.5, 40], // starting position [lng, lat]
+          zoom: 3, // starting zoom
+          preserveDrawingBuffer : true
+        });
