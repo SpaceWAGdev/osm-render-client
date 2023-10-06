fd5a101c524429142eecb5bccc542d97e40044e0
starlight_caffeine <starlight-caffeine@posteo.org>
2023-10-06 14:34:42 +0200

    Prepare Printing System

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function preparePrint() {
     document.querySelector(".right").classList.toggle("hidden");
     document.querySelector(".left").classList.toggle("full-width");
+
+    let map_png = map.getCanvas().toDataURL();
+    let img = new Image;
+    img.src = map_png;
+    document.querySelector(".left").appendChild(img);
+    document.querySelector("#map").classList.toggle("hidden");
+
     print();
+    document.querySelector("#")
+
     document.querySelector(".right").classList.toggle("hidden");
     document.querySelector(".left").classList.toggle("full-width");
 }
@@ -9,7 +18,7 @@ function preparePrint() {
 console.log(maplibregl);
         var map = new maplibregl.Map({
           container: 'map',
-          style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+          style: 'map-styles/default_style.json', // stylesheet location
           center: [-74.5, 40], // starting position [lng, lat]
           zoom: 3, // starting zoom
           preserveDrawingBuffer : true
