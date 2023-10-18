68940539e713c97d1d9e8e36f4f055a501a12302
starlight_caffeine <starlight-caffeine@posteo.org>
2023-10-18 09:08:25 +0200

    Finalize map style select and fix preparePrint

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function preparePrint() {
   document.querySelector("#map").classList.toggle("hidden");
 
   print();
-  document.querySelector("#")
-  document.querySelector(".right").classList.toggle("hidden");
+  document.querySelector(".left").removeChild(img);
   document.querySelector(".left").classList.toggle("full-width");
+  document.querySelector(".right").classList.toggle("hidden");
+  document.querySelector("#map").classList.toggle("hidden");
 }
 
 console.log(maplibregl);
@@ -24,13 +25,20 @@ var map = new maplibregl.Map({
 });
 
 function updateMapStyle(element) {
+  console.log(element.dataset.maptype)
   switch (element.dataset.maptype) {
     case "default":
-      
+      map.setStyle("https://api.maptiler.com/maps/basic-v2/style.json?key=yH0zK1xSaQ1U4Zu8y0wL")
       break;
     case "toner":
+      map.setStyle("https://api.maptiler.com/maps/toner-v2/style.json?key=yH0zK1xSaQ1U4Zu8y0wL")
       break;
+    case "satellite":
+      map.setStyle("https://api.maptiler.com/maps/satellite/style.json?key=yH0zK1xSaQ1U4Zu8y0wL")
+      break;
+    case "detailed":
+      map.setStyle("https://api.maptiler.com/maps/streets-v2/style.json?key=yH0zK1xSaQ1U4Zu8y0wL")
     default:
-      break;
+      map.setStyle("https://api.maptiler.com/maps/basic-v2/style.json?key=yH0zK1xSaQ1U4Zu8y0wL")
   }
 }
